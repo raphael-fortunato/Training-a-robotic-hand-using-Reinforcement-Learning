33d4e7b36e1fe7a190e7ebdd11735d733a3a6a43
Raphael Furtunato <r.furtunato@students.uu.nl>
2019-12-17 13:05:01 +0100

    build the beginning of the agent class

--- Main.py
+++ Main.py
@@ -1,9 +1,52 @@
 import gym 
+import pdb
+
+import numpy as np
+from collections import deque
+
+import torch
+
+from rl.memory import SequentialMemory
+
+from models import Actor, Critic
+
+
+class Agent:
+    def __init__(self, env, env_params):
+        self.env= env
+        self.env_params = env_params
+        self.hidden_neurons = 256
+        # networks
+        self.actor = Actor(self.env_params, self.hidden_neurons)
+        self.critic = Critic(self.env_params, self.hidden_neurons)
+        # target networks used to predict env actions with
+        self.actor_target = Actor(self.env_params, self.hidden_neurons)
+        self.critic_target = Critic(self.env_params, self.hidden_neurons)
+        if torch.cuda.is_available():
+            self.actor.cuda()
+            self.critic.cuda()
+            self.actor_target.cuda()
+            self.critic_target.cuda()
+        self.actor_optim = torch.optim.Adam(self.actor.parameters(), lr=0.001)
+        self.critic_optim = torch.optim.Adam(self.critic.parameters(), lr=0.001)
+        self.memory = SequentialMemory(limit=1_000_000, window_length=1)
+
+
+
+
+
+def get_params(env):
+    obs = env.reset()
+    params = {'observation': obs['observation'].shape[0],
+            'goal': obs['desired_goal'].shape[0],
+            'action': env.action_space.shape[0],
+            'max_action': env.action_space.high[0],
+            }
+    params['max_timesteps'] = env._max_episode_steps
+    return params
+
 
 env = gym.make('HandManipulateBlock-v0')
-
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+env_param = get_params(env)
+agent = Agent(env,env_param)
 env.close()
---
+++ models.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Actor(nn.Module):
+    def __init__(self,env_params, hidden_neurons):
+       super(Actor, self).__init__() 
+       self.max_action = env_params['max_action']
+       self.fc1 = nn.Linear(env_params['observation']+ env_params['goal'],hidden_neurons )
+       self.fc2 = nn.Linear(hidden_neurons, hidden_neurons)
+       self.fc3 = nn.Linear(hidden_neurons, hidden_neurons)
+       self.output = nn.Linear(hidden_neurons, env_params['action'])
+
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        action = self.max_action * F.tanh(self.output(x))
+        return action
+
+
+class Critic(nn.Module):
+    def __init__(self ,env_params, hidden_neurons):
+        super(Critic, self).__init__()
+        self.max_action = env_params['max_action']
+        self.fc1 = nn.Linear(env_params['observation'] + env_params['goal']+ env_params['action'],hidden_neurons )
+        self.fc2 = nn.Linear(hidden_neurons, hidden_neurons)
+        self.fc3 = nn.Linear(hidden_neurons, hidden_neurons)
+        self.q = nn.Linear(hidden_neurons, 1)
+
+    def forward(self, x, action):
+        x = torch.cat([x, action/ self.max_action], dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        q_value = self.q(x)
+        return q_value
