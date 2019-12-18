b643bf0a6749eb7b686e646c35e4883765473d2c
Raphael Furtunato <r.furtunato@students.uu.nl>
2019-12-18 14:48:34 +0100

    added random noise to action()

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import gym
 import pdb
 
 import numpy as np
+import random
 from collections import deque
 
 import torch
@@ -13,11 +14,12 @@ from models import Actor, Critic
 from normalizer import Normalizer
 
 class Agent:
-    def __init__(self, env, env_params, n_episodes ,screen=False,save_path='models'):
+    def __init__(self, env, env_params, n_episodes, noise_eps ,screen=False,save_path='models'):
         self.env= env
         self.env_params = env_params
         self.episodes = n_episodes
         self.hidden_neurons = 256
+        self.noise_eps = noise_eps
         # networks
         self.actor = Actor(self.env_params, self.hidden_neurons)
         self.critic = Critic(self.env_params, self.hidden_neurons)
@@ -43,9 +45,21 @@ class Agent:
 
     def Action(self, step):
         with torch.no_grad():
-            action = self.actor_target.forward(step)
-        # add noise 
-        return action.detach().cpu().numpy().squeeze()
+            action = self.actor_target.forward(step).detach().cpu().numpy()
+            action +=self.noise_eps * self.env_params['max_action'] * np.random.randn(*action.shape)
+            action = np.clip(action, -self.env_params['max_action'], self.env_params['max_action'])
+            # random actions...
+            random_actions = np.random.uniform(low=-self.env_params['max_action'], high=self.env_params['max_action'], \
+                                            size=self.env_params['action'])
+            # choose if use the random actions
+            # need to specify random.uniform
+            action += np.random.binomial(1, random.uniform(0,1), 1)[0] * (random_actions - action)
+        return action.squeeze()
+
+
+    def Update(self):
+        batch  = self.memory.sample(32)
+        pass
 
     def Learn(self):
         for episode in range(self.episodes):
@@ -54,16 +68,19 @@ class Agent:
                 if self.screen:
                     env.render()
                 #NORMALIZE EVERY INPUT 
-                pdb.set_trace()
-                norm = Normalizer(-10, 10)
-                test = norm.normalize(state)
+                #pdb.set_trace()
+                #norm = Normalizer(-10, 10)
+                #test = norm.normalize(state)
                 action = self.Action(state)
-                print(f"ACTION: {action}")
+
                 nextstate, reward, done, info = env.step(action)
+                self.memory.append(state, action, reward, done, nextstate)
                 state = nextstate
                 if done:
                     break
-    
+
+
+
     def SaveModel(self):
         torch.save(self.actor.state_dict(), self.model_path[0])
         torch.save(self.critic.state_dict(), self.model_path[1])
@@ -83,6 +100,6 @@ def get_params(env):
 
 env = gym.make('HandManipulateBlock-v0')
 env_param = get_params(env)
-agent = Agent(env,env_param, 3, screen=True)
+agent = Agent(env,env_param, 3, 1., screen=True)
 agent.Learn()
 env.close()
