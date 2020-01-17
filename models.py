c49b9a4b79d4ac7a70b71990e5867304b8e4aad5
4170512 <r.furtunato@students.uu.nl>
2020-01-17 02:02:19 +0100

    added weight init

--- models.py
+++ models.py
@@ -6,13 +6,23 @@ import pdb
 
 class Actor(nn.Module):
     def __init__(self,env_params, hidden_neurons):
-       super(Actor, self).__init__() 
-       self.max_action = env_params['max_action']
-       self.fc1 = nn.Linear(env_params['observation']+ env_params['goal'],hidden_neurons )
-       self.fc2 = nn.Linear(hidden_neurons, hidden_neurons)
-       self.fc3 = nn.Linear(hidden_neurons, hidden_neurons)
-       self.output = nn.Linear(hidden_neurons, env_params['action'])
+        super(Actor, self).__init__() 
+        self.max_action = env_params['max_action']
+        self.fc1 = nn.Linear(env_params['observation']+ env_params['goal'],hidden_neurons )
+        self.fc2 = nn.Linear(hidden_neurons, hidden_neurons)
+        self.fc3 = nn.Linear(hidden_neurons, hidden_neurons)
+        self.output = nn.Linear(hidden_neurons, env_params['action'])
+        f1 = 1 / np.sqrt(self.fc1.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc1.weight.data, -f1,f1)
+        torch.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
+        
+        f2 = 1 / np.sqrt(self.fc2.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc2.weight.data, -f2,f2)
+        torch.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
 
+        f3 = 1 / np.sqrt(self.fc3.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc3.weight.data, -f3,f3)
+        torch.nn.init.uniform_(self.fc3.bias.data, -f3, f3)
 
     def forward(self, x):
         if type(x) is not torch.Tensor:
@@ -35,6 +45,17 @@ class Critic(nn.Module):
         self.fc2 = nn.Linear(hidden_neurons, hidden_neurons)
         self.fc3 = nn.Linear(hidden_neurons, hidden_neurons)
         self.q = nn.Linear(hidden_neurons, 1)
+        f1 = 1 / np.sqrt(self.fc1.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc1.weight.data, -f1,f1)
+        torch.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
+        
+        f2 = 1 / np.sqrt(self.fc2.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc2.weight.data, -f2,f2)
+        torch.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
+
+        f3 = 1 / np.sqrt(self.fc3.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc3.weight.data, -f3,f3)
+        torch.nn.init.uniform_(self.fc3.bias.data, -f3, f3)        
 
     def forward(self, x, action):
         x = torch.cat([x, action/ self.max_action], dim=1)
