f40a2a6a98512b0ee50ee0f923d4572db36d5b5f
4170512 <r.furtunato@students.uu.nl>
2020-01-23 15:23:14 +0100

    added os path

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ import gym
 import numpy as np
 import torch
 import argparse
+import os 
 
 from models import Actor
 
@@ -11,7 +12,7 @@ def Normalize(state, o_mean, o_std, g_mean, g_std):
     o_clip = np.clip(o, -CLIP_OBS, CLIP_OBS)
     g_clip = np.clip(g, -CLIP_OBS, CLIP_OBS)
     o_norm = np.clip((o_clip - o_mean) / (o_std), -CLIP_RANGE, CLIP_RANGE)
-    g_norm = np.clip((g_clip - g_mean) / (g_std), -CLIP_RANGE, clip)
+    g_norm = np.clip((g_clip - g_mean) / (g_std), -CLIP_RANGE, CLIP_RANGE)
     inputs = np.concatenate([o_norm, g_norm])
     inputs = torch.tensor(inputs, dtype=torch.float32)
     return inputs
@@ -33,16 +34,16 @@ CLIP_RANGE = 5
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--demo-length',type=int, default=20, help='number of demo episodes to run')
+    parser.add_argument('--demo-length',type=int, default=200000, help='number of demo episodes to run')
     parser.add_argument('--distance', action='store_true', help='shows model with the distance version of per')
     parser.add_argument('--impact', action='store_true', help='shows model with the impact version of per')
     args = parser.parse_args()
     if args.distance:
-        model_path = "models/HandManipulateBlock-v0/distance/model.pt"
+        model_path = os.path("models/HandManipulateBlock-v0/distance/model.pt")
     elif args.impact:
-        model_path = "models/HandManipulateBlock-v0/impact/model.pt"
+        model_path = os.path("models/HandManipulateBlock-v0/impact/model.pt")
     else:
-        model_path = "models/HandManipulateBlock-v0/normal/model.pt"
+        model_path = os.path("models/HandManipulateBlock-v0/normal/model.pt")
 
     env = gym.make('HandManipulateBlock-v0')
     env_params = get_params(env)
